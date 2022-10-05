51e41ce422649999c8c0fe2481687e0531fe9071
Busu <busurmanbek000@gmail.com>
2022-10-05 18:45:14 +0600

    update

--- src/Service/DeliveryCompanyService.java
+++ src/Service/DeliveryCompanyService.java
@@ -2,8 +2,6 @@ package Service;
 
 import entities.Customer;
 import entities.DeliveryCompany;
-import entities.Load;
-import entities.Order;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,9 +9,10 @@ import java.util.Scanner;
 public class DeliveryCompanyService {
     private ArrayList<DeliveryCompany> deliveryCompanies;
 
-    public void acceptOrder() {
-        Scanner s = new Scanner(System.in);
+    public void acceptOrder(Customer customer) {
+        /* remove in main branch
         System.out.print("Enter customer name: ");
+        Scanner s = new Scanner(System.in);
         String customerName = s.nextLine();
         System.out.print("Enter height: ");
         int height = s.nextInt();
@@ -21,25 +20,36 @@ public class DeliveryCompanyService {
         int width = s.nextInt();
         System.out.print("Enter weight: ");
         int weight = s.nextInt();
-
-        Customer customer = new Customer(customerName, new Order(0, new Load(height, width, weight)));
-
-        if (!deliveryCompanies.isEmpty()) {
+        System.out.print("Enter length: ");
+        int length = s.nextInt();
+        Customer customer = new Customer(customerName, new Order(0, new Load(height, width, weight, length)));
+*/
+        Scanner s = new Scanner(System.in);
+        if (deliveryCompanies.isEmpty()) {
+            System.out.println("No companies created.");
+        } else {
             System.out.println("Choose delivery company:");
             for (int i = 0; i < deliveryCompanies.size(); i++) {
                 System.out.printf("%d: %s, price: %d", i + 1, deliveryCompanies.get(i).getDeliveryCompanyName(), deliveryCompanies.get(i).getPricePerKilogram());
             }
             int target = s.nextInt();
-            if ((customer.getOrder().getLoad().getHeight() +
-                    customer.getOrder().getLoad().getWidth()) * 2 <=
-                    deliveryCompanies.get(target).getMaxCapacityPerSquareCentimeter())
-            {
-                deliveryCompanies.get(s.nextInt()).addCustomer(customer);
-            }
+            if (target < 0 || target >= deliveryCompanies.size())
+                System.out.println("Invalid target");
             else {
-                System.out.println("Max order capacity for this company is reached.");
+                if ((customer.getOrder().getLoad().getHeight() *
+                        customer.getOrder().getLoad().getWidth() *
+                        customer.getOrder().getLoad().getLength()) >
+                        deliveryCompanies.get(target).getMaxCapacityPerSquareCentimeter()) {
+                    System.out.println("Max order capacity for this company is reached.");
+                } else {
+                    System.out.println(customer.getOrder().getOrderFee());
+                    customer.getOrder().setOrderFee(deliveryCompanies.get(target).getPricePerKilogram() *
+                            customer.getOrder().getLoad().getWeight());
+                    System.out.println(customer.getOrder().getOrderFee());
+                    deliveryCompanies.get(target).addCustomer(customer);
+                }
             }
-        } else System.out.println("No companies created.");
+        }
 
     }
 }
--- src/entities/Load.java
+++ src/entities/Load.java
@@ -4,11 +4,21 @@ public class Load {
     private int height;
     private int width;
     private int weight;
+    private int length;
 
-    public Load(int height, int width, int weight) {
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Load(int height, int width, int weight, int length) {
         this.height = height;
         this.width = width;
         this.weight = weight;
+        this.length = length;
     }
 
     public int getHeight() {
